a89780cb7f78832034f7d9c8da77518aa14544a2
Ashleigh Pook <ashleighpook@gmail.com>
2013-09-26 23:26:11 +0100

    Automatically convert tabs to spaces.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,12 +2,22 @@
 
 startPath=`pwd`
 
-# Generate Abacus.cs
 cd ../generate/Abacus
+
+# tabs to spaces
+find ./ ! -type d ! -name _tmp_ -exec sh -c 'expand -t 4 {} > _tmp_ && mv _tmp_ {}' \;
+
+# Generate Abacus.cs
 mono-t4 Abacus.tt -o ../../source/Abacus.cs
+
 cd $startPath
 
-# Generate Tests.cs
 cd ../generate/Abacus.Tests
+
+# tabs to spaces
+find ./ ! -type d ! -name _tmp_ -exec sh -c 'expand -t 4 {} > _tmp_ && mv _tmp_ {}' \;
+
+# Generate Tests.cs
 mono-t4 Abacus.Tests.tt -o ../../source/Abacus.Tests.cs
+
 cd $startPath
