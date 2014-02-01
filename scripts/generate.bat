208b339964cc69a404e187ddde897f58fddfd366
Ashleigh Pook <ashleighpook@gmail.com>
2014-02-01 22:39:55 +0000

    Updated generate script so that Linq works in Text Templates.

--- scripts/generate.bat
+++ scripts/generate.bat
@@ -3,13 +3,13 @@ cd ../generate/
 :: Abacus
 :: ------
 cd Abacus/
-TextTransform.exe Abacus.tt -o ../../source/Abacus.cs
+TextTransform.exe Abacus.tt -o ../../source/Abacus.cs -r System.Core.dll
 cd ../
 
 :: Abacus.Tests
 :: ------------
 cd Abacus.Tests/
-TextTransform.exe Abacus.Tests.tt -o ../../source/Abacus.Tests.cs
+TextTransform.exe Abacus.Tests.tt -o ../../source/Abacus.Tests.cs -r System.Core.dll
 cd ../
 
 cd ../
