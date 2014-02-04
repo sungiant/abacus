76fe154778208c021c4f40758d99b3c261a9faad
Ashleigh Pook <ashleighpook@gmail.com>
2014-02-04 09:53:02 +0000

    Make sure the scripts return 1 on a fail.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ startPath=`pwd`
 
 cd ../build_examples/xs.mono40/Abacus/
 
-xbuild "Abacus (xs.mono40).csproj" /p:Configuration=Debug
-xbuild "Abacus (xs.mono40).csproj" /p:Configuration=Release
+xbuild "Abacus (xs.mono40).csproj" /p:Configuration=Debug && exit 1
+xbuild "Abacus (xs.mono40).csproj" /p:Configuration=Release && exit 1
 
 cd $startPath
