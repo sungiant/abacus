d05f5fb6abdf27029bc7afff27e8b51b7ea3916f
Travis CI <travis@travis-ci.org>
2020-09-08 20:13:38 +0000

    Deploy code docs to GitHub Pages Travis build: 237
    Commit: ff525cedd1df16525ca06269a27e09cd96224dc8

--- search/all_2.js
+++ search/all_2.js
@@ -1,7 +1,11 @@
 var searchData=
 [
+  ['maths',['Maths',['../classAbacus_1_1DoublePrecision_1_1Maths.html',1,'Abacus::DoublePrecision']]],
+  ['maths',['Maths',['../classAbacus_1_1Fixed64Precision_1_1Maths.html',1,'Abacus::Fixed64Precision']]],
+  ['maths',['Maths',['../classAbacus_1_1Fixed32Precision_1_1Maths.html',1,'Abacus::Fixed32Precision']]],
+  ['maths',['Maths',['../classAbacus_1_1SinglePrecision_1_1Maths.html',1,'Abacus::SinglePrecision']]],
   ['matrix44',['Matrix44',['../structAbacus_1_1Fixed64Precision_1_1Matrix44.html',1,'Abacus::Fixed64Precision']]],
   ['matrix44',['Matrix44',['../structAbacus_1_1SinglePrecision_1_1Matrix44.html',1,'Abacus::SinglePrecision']]],
-  ['matrix44',['Matrix44',['../structAbacus_1_1Fixed32Precision_1_1Matrix44.html',1,'Abacus::Fixed32Precision']]],
-  ['matrix44',['Matrix44',['../structAbacus_1_1DoublePrecision_1_1Matrix44.html',1,'Abacus::DoublePrecision']]]
+  ['matrix44',['Matrix44',['../structAbacus_1_1DoublePrecision_1_1Matrix44.html',1,'Abacus::DoublePrecision']]],
+  ['matrix44',['Matrix44',['../structAbacus_1_1Fixed32Precision_1_1Matrix44.html',1,'Abacus::Fixed32Precision']]]
 ];
